389c511412043421dbefa4c193d820e187984c3b
alexkazancew <alexkazancew@yandex.ru>
2017-10-16 21:15:25 +0500

    Обновил GUI

---
+++ InverseTest/MathUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InverseTest
+{
+    public static class MathUtils
+    {
+        public static double ToRadians(double angle)
+        {
+            return (Math.PI / 180) * angle; 
+        }
+    }
+}
