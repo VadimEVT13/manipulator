a324d5f20ab564ee4079bbf66a70dbf320c9bdde
kashinoleg <kashin.oleg@list.ru>
2018-04-20 14:59:15 +0500

    Перенос форм

---
+++ InverseTest/GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using log4net;
+
+namespace InverseTest.GUI.ViewModels
+{
+    /// <summary>
+    /// Implements INotifyPropertyChanged for all ViewModel
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        protected static readonly ILog Log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
