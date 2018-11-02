60385c38547c38b2216f40579153896c5d772156
DESKTOP-KI1H62N\Vadim <artemew.v@yandex.ru>
2018-11-02 19:44:39 +0500

    Add JsonReader/JsonWriter

--- InverseTest/GUI/Readers/JsonReader.cs
+++ InverseTest/GUI/Readers/JsonReader.cs
@@ -1,7 +1,10 @@
 ﻿using InverseTest.Path;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NLog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +20,7 @@ namespace InverseTest.GUI.Readers
 
         public static List<ScanPoint> Read(string fileName)
         {
-            return new List<ScanPoint>();
+            return JsonConvert.DeserializeObject<List<ScanPoint>>(fileName);
         }
     }
 }
--- InverseTest/GUI/Writers/JsonWriter.cs
+++ InverseTest/GUI/Writers/JsonWriter.cs
@@ -1,8 +1,10 @@
 ﻿using InverseTest.Path;
+using Newtonsoft.Json;
 using NLog;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +20,13 @@ namespace InverseTest.GUI.Writers
 
         public static void Write(List<ScanPoint> pointsList, string fileName)
         {
+            string json = JsonConvert.SerializeObject(pointsList, Formatting.Indented);
 
+            // Без дозаписи?
+            using (StreamWriter sw = new StreamWriter(fileName, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(json);
+            }
         }
     }
 }
